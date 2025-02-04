58f19b4175c244e02354b1bd6ec8f083040e3fdc
equalsAndy <equalsandy@gmail.com>
2025-02-03 20:29:56 -0700

    added get

--- routes/oauth.js
+++ routes/oauth.js
@@ -9,6 +9,22 @@ const oauth = new OAuth2Server({
   model: require("../models/oauthModel"), // Your OAuth logic
 });
 
+
+router.get("/authorize", (req, res) => {
+  res.send(`
+    <form method="post" action="/oauth/authorize">
+      <input type="hidden" name="response_type" value="code" />
+      <input type="hidden" name="client_id" value="alexa-client" />
+      <input type="hidden" name="redirect_uri" value="YOUR_ALEXA_REDIRECT_URL" />
+      <input type="text" name="username" placeholder="Username" />
+      <input type="password" name="password" placeholder="Password" />
+      <button type="submit">Authorize</button>
+    </form>
+  `);
+});
+
+
+
 router.post("/token", async (req, res) => {
   const request = new Request(req);
   const response = new Response(res);
