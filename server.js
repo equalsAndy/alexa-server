b0928e8d647070e96618a7db3762d3ce80e78e28
equalsAndy <equalsandy@gmail.com>
2025-02-03 21:01:53 -0700

    added ssl

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const https = require("https");
+const fs = require("fs");
+const express = require("express");
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +13,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Load OAuth & API Routes
 const oauthRoutes = require("./routes/oauth");
 const apiRoutes = require("./routes/api");
+
+// Load SSL certificate
+const options = {
+  key: fs.readFileSync("/etc/ssl/private/selfsigned.key"), // Change this if using Certbot
+  cert: fs.readFileSync("/etc/ssl/certs/selfsigned.crt")   // Change this if using Certbot
+};
+
+// Use HTTPS
+https.createServer(options, app).listen(443, () => {
+  console.log("Server running on HTTPS port 443");
+});
 
 app.use("/oauth", oauthRoutes);
 app.use("/api", apiRoutes);
